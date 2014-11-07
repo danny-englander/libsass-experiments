41ff61ee1e3096a4e3bc6a295e8117f8220b61ff
Danny Englander <highrockmedia@gmail.com>
2014-11-07 06:07:29 -0800

    Adding in Neat

--- gruntfile.js
+++ gruntfile.js
@@ -3,30 +3,28 @@ module.exports = function (grunt) {
     pkg: grunt.file.readJSON('package.json'),
     watch: {
       options: {
-        includePaths: require('node-bourbon').includePaths
+        includePaths: require('node-neat').includePaths
       },
       sass: {
         files: ['sass/**/*.{scss,sass}', 'sass/_partials/**/*.{scss,sass}'],
         tasks: ['sass:dist']
-      },
+      }, //sass
       livereload: {
         files: ['*.html', '*.php', 'js/**/*.{js,json}', 'css/*.css', 'img/**/*.{png,jpg,jpeg,gif,webp,svg}'],
-        options: {
-          livereload: true
-        }
-      }
-    },
+        options: { livereload: true }
+      } // livereload
+    }, //watch
     sass: {
       dist: {
         options: {
-          includePaths: require('node-bourbon').includePaths,
+          includePaths: require('node-neat').includePaths,
           sourceMap: true,
           outputStyle: 'expanded'
         },
         files: {
           'css/styles.css': 'sass/styles.scss'
-        }
-      }
+        } //files
+      } //dist
     }
   });
   grunt.loadNpmTasks('grunt-sass');
