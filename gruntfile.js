ae69de26ca1a580ea410e193a2f3a5c0baa5771c
Danny Englander <highrockmedia@gmail.com>
2014-11-06 20:16:45 -0800

    adding in Bourbon

--- gruntfile.js
+++ gruntfile.js
@@ -1,27 +1,27 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     watch: {
+      options: {
+        includePaths: require('node-bourbon').includePaths
+      },
       sass: {
-        files: ['sass/**/*.{scss,sass}','sass/_partials/**/*.{scss,sass}'],
+        files: ['sass/**/*.{scss,sass}', 'sass/_partials/**/*.{scss,sass}'],
         tasks: ['sass:dist']
       },
       livereload: {
-        files: ['*.html', '*.php', 'js/**/*.{js,json}', 'css/*.css','img/**/*.{png,jpg,jpeg,gif,webp,svg}'],
+        files: ['*.html', '*.php', 'js/**/*.{js,json}', 'css/*.css', 'img/**/*.{png,jpg,jpeg,gif,webp,svg}'],
         options: {
           livereload: true
         }
       }
     },
     sass: {
-      options: {
-        sourceMap: true,
-        outputStyle: 'expanded'
-      },
       dist: {
         options: {
-          loadPath: require('node-bourbon').includePaths,
-          style: 'expanded'
+          includePaths: require('node-bourbon').includePaths,
+          sourceMap: true,
+          outputStyle: 'expanded'
         },
         files: {
           'css/styles.css': 'sass/styles.scss'
@@ -29,9 +29,7 @@ module.exports = function(grunt) {
       }
     }
   });
-  grunt.registerTask('default', ['sass:dist', 'watch']);
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  //grunt.loadNpmTasks('node-bourbon');
-  //grunt.loadNpmTasks('node-neat');
+  grunt.registerTask('default', ['sass:dist', 'watch']);
 };
